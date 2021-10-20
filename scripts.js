c31b9a29cc067249981a2cac4f0c2f84cfbc08f6
jiji <jijigopal2000@gmail.com>
2021-10-20 21:35:21 +0530

    calculator app

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// function displayValue(num){
+//     let res=document.querySelector("#result")   // arrow fuction = var displayValue=num()=>result.value+=num
+//     res.value+=num;
+// }
+
+var displayValue=(num)=>result.value+=num
+
+// function clearBox(){   // arrow function = var clearBox=()=>result.value=""
+//     result.value="";
+    
+// }
+var clearBox=()=>result.value=""
+
+// function evaluateExpression(){
+//     let expr=result.value;
+//     let out=eval(expr);   // eval() is a javascript built in function to evaluate an expression
+//     result.value=out; 
+//}    
+// arrow function
+var evaluateExpression=()=>result.value=eval(result.value)
+
+/*
+bank app
+reg form
+
+first name:
+account num:
+password:
+confirmpassword:
+
+
+login
+
+accno:
+password
+login button
+*/
